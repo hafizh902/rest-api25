e1803d5a1797b8d5de43a2476b125148e94502ec
hafizh902 <hafizhiftikharutama@gmail.com>
2025-09-03 11:06:37 +0800

    finish Backend

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductCollection;
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 
 // use GuzzleHttp\Psr7\Response;
@@ -17,7 +19,45 @@ class ProductController extends Controller
         // Mengambil semua produk dari database
         $products = Product::latest()->paginate(10);
         return response()->json(new ProductCollection($products), Response::HTTP_OK);
+    }
 
-   
+    public function store(ProductRequest $product){
+        $product = Product::create($product->validated());
+        return response()->json([
+            'status'   => true,
+            'message'  => 'Produk Berhasil Ditambahkan',
+            'data'     => new ProductResource($product)
+        ], Response::HTTP_CREATED);
+    }
+
+    public function show($id){
+        $product = Product::findOrFail($id);
+        return response()->json([
+            'status'   => true,
+            'message'  => 'Detail Produk',
+            'data'     => new ProductResource($product)
+        ], Response::HTTP_OK);
+    }
+
+    public function update(ProductRequest $request, $id){
+        $product = Product::findOrFail($id);
+        $product->update($request->validated());
+        return response()->json([
+            'status'   => true,
+            'message'  => 'Produk Berhasil Diupdate',
+            'data'     => new ProductResource($product)
+        ], Response::HTTP_OK);
+    }
+
+    public function destroy(Product $product){
+        $product->delete();
+        return response()->json([
+            'status'   => true,
+            'message'  => 'Produk Berhasil Dihapus',
+            'data'     => new ProductResource($product)
+        ], Response::HTTP_OK);
+
     }
 }
+
+
